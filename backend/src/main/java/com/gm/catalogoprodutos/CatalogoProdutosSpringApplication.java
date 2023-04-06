ace8d1a5741b3dba9b8f26678e8037680c961d47
Gessica Mendonça <gm.gessica@outlook.com>
2023-04-06 17:42:35 -0300

    criação do projeto

---
+++ backend/src/main/java/com/gm/catalogoprodutos/CatalogoProdutosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gm.catalogoprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProdutosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdutosSpringApplication.class, args);
+	}
+
+}
