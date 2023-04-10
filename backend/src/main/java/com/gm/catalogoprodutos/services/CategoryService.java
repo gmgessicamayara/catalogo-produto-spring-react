cb3328f6d1e26a804310fdc09c65e8796f5cac8c
Gessica Mendonça <gm.gessica@outlook.com>
2023-04-10 20:39:16 -0300

    Pagination

--- backend/src/main/java/com/gm/catalogoprodutos/services/CategoryService.java
+++ backend/src/main/java/com/gm/catalogoprodutos/services/CategoryService.java
@@ -1,14 +1,14 @@
 package com.gm.catalogoprodutos.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,20 +25,20 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
 
-		List<Category> categorias = repository.findAll();
-		return categorias.stream().map(item -> new CategoryDTO(item)).collect(Collectors.toList());
+		Page<Category> categorias = repository.findAll(pageRequest);
+		return categorias.map(item -> new CategoryDTO(item));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
-		
+
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(()-> new ResourceNotFoundException ("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
-	
+
 	@Transactional
 	public CategoryDTO insert(CategoryDTO dto) {
 		Category entity = new Category();
@@ -54,9 +54,8 @@ public class CategoryService {
 			entity.setName(dto.getName());
 			entity = repository.save(entity);
 			return new CategoryDTO(entity);
-		}
-		catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id not found: "+ id);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
 		}
 
 	}
@@ -64,11 +63,9 @@ public class CategoryService {
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-		}
-		catch (EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException("Id not found: "+ id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found: " + id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Integrity violation");
 		}
 	}
