b7a2214b206b25cff6f702839652d43c159bb854
Gessica Mendonça <gm.gessica@outlook.com>
2023-04-10 16:37:53 -0300

    Database seed

--- backend/src/main/java/com/gm/catalogoprodutos/services/CategoryService.java
+++ backend/src/main/java/com/gm/catalogoprodutos/services/CategoryService.java
@@ -2,8 +2,10 @@ package com.gm.catalogoprodutos.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gm.catalogoprodutos.Repositories.CategoryRepository;
 import com.gm.catalogoprodutos.entities.Category;
@@ -14,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
