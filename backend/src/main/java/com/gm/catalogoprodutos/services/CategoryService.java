33bbc23ea9a4531a1381088bd9976a4d3cacf621
Gessica Mendonça <gm.gessica@outlook.com>
2023-04-10 16:48:01 -0300

    CategoryDTO

--- backend/src/main/java/com/gm/catalogoprodutos/services/CategoryService.java
+++ backend/src/main/java/com/gm/catalogoprodutos/services/CategoryService.java
@@ -1,23 +1,27 @@
 package com.gm.catalogoprodutos.services;
 
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gm.catalogoprodutos.Repositories.CategoryRepository;
+import com.gm.catalogoprodutos.dto.CategoryDTO;
 import com.gm.catalogoprodutos.entities.Category;
 
-@Service 
+@Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+
+		List<Category> categorias = repository.findAll();
+
+		return categorias.stream().map(item -> new CategoryDTO(item)).collect(Collectors.toList());
 	}
 }
